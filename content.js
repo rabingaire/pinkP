f7b21a041b18a18983256bce48c7f496c861cb0e
Rabin Gaire <rabingaire20@gmail.com>
2017-12-07 00:19:33 +0545

    initial commit

---
+++ content.js
@@ -0,0 +1,6 @@
+let paragraphs = document.getElementsByTagName('p');
+
+for (element of paragraphs) {
+    element.style['background-color'] = '#FF00FF';
+    element.style['color'] = '#FFFFFF';
+} 
